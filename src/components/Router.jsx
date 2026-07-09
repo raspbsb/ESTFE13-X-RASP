05b4165d913dfced2d635bdbbdccf01adae02045
Raspbsb <unionbjh@naver.com>
2026-07-09 11:08:37 +0900

    refactor: update Router component to accept isLoggedIn as a prop and remove local state

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
 import { Route, Routes } from "react-router";
 
-function Router() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+function Router({ isLoggedIn }) {
   return (
     <>
       <Routes>
