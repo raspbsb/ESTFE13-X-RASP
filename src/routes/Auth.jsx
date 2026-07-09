688ec03086c8db9251d6803ac818d5116faa9c56
Raspbsb <unionbjh@naver.com>
2026-07-09 13:10:42 +0900

    refactor: enhance Auth component with form submission handling and user authentication

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { Box, Typography, TextField, Button } from "@mui/material";
+import { Box, Typography, TextField, Button, Divider } from "@mui/material";
+import { authService } from "../firebase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 function Auth() {
   const [newAccount, setNewAccount] = useState(true);
@@ -7,8 +9,7 @@ function Auth() {
     email: "",
     password: "",
   });
-
-  console.log(form);
+  const auth = authService;
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -19,12 +20,43 @@ function Auth() {
     }));
   };
 
+  const onSubmit = e => {
+    e.preventDefault();
+    if (newAccount) {
+      // 회원가입
+      createUserWithEmailAndPassword(auth, form.email, form.password)
+        .then(userCredential => {
+          // Signed up
+          const user = userCredential.user;
+          // ...
+        })
+        .catch(error => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    } else {
+      // 로그인
+      signInWithEmailAndPassword(auth, form.email, form.password)
+        .then(userCredential => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+        })
+        .catch(error => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    }
+  };
+
   return (
     <>
       <Typography variant="h2" component="h2">
-        Login Form
+        {newAccount ? "회원가입 폼" : "로그인 폼"}
       </Typography>
-      <Box component="form" sx={{ mt: 2 }}>
+      <Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
         <TextField
           fullWidth
           label="Email address"
@@ -45,7 +77,21 @@ function Auth() {
           onChange={handleChange}
         />
         <Button sx={{ mt: 2 }} type="submit" variant="contained">
-          Login
+          {newAccount ? "회원가입" : "로그인"}
+        </Button>
+
+        <Divider sx={{ my: 3 }} />
+
+        {/* 로그인/회원가입 전환 버튼 */}
+        <Button
+          sx={{ mt: 2 }}
+          type="button"
+          variant="contained"
+          onClick={() => {
+            setNewAccount(prev => !prev);
+          }}
+        >
+          {newAccount ? "로그인으로 전환" : "회원가입으로 전환"}
         </Button>
       </Box>
     </>
