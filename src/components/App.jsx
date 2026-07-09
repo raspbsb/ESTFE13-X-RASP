7ef64f5200e00f6d54150bfe88b86a22f71eb5ba
Raspbsb <unionbjh@naver.com>
2026-07-09 13:10:37 +0900

    refactor: implement authentication state handling in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,24 @@ import "./App.css";
 import { useState } from "react";
 import { authService } from "../firebase";
 import { Container } from "@mui/material";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
-  console.log(authService.currentUser);
+  // console.log(authService.currentUser);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const auth = authService;
+
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      // User is signed in
+      const uid = user.uid;
+      setIsLoggedIn(true);
+    } else {
+      // User is signed out
+      setIsLoggedIn(false);
+    }
+  });
+
   return (
     <>
       <Container>
