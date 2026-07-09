9c2cf46755a2fff7f5ce2ddad251a832c0d844a5
Raspbsb <unionbjh@naver.com>
2026-07-09 11:08:22 +0900

    refactor: restructure App component layout and initialize authentication state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import Router from "./Router";
 import "./App.css";
+import { useState } from "react";
+import { authService } from "../firebase";
+import { Container } from "@mui/material";
 
 function App() {
+  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <>
-      <h1>ESTFE-X-RASP</h1>
-      <Router />
+      <Container>
+        <h1>ESTFE-X-RASP</h1>
+        <Router isLoggedI={isLoggedIn} />
+      </Container>
     </>
   );
 }
