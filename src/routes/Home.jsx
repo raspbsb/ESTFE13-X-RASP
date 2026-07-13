e54c2d5e6183704f36651d679255bb853f37c8dd
Raspbsb <unionbjh@naver.com>
2026-07-13 11:19:55 +0900

    feat: onSnapShot 함수를 활용해 실시간 업데이트 기능 추가

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,51 @@
-import { Box, Typography, TextField, Button, Divider } from "@mui/material";
-import { collection, addDoc, updateDoc, serverTimestamp } from "firebase/firestore";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Divider,
+  useScrollTrigger,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  query,
+  where,
+  getDocs,
+  orderBy,
+  limit,
+  onSnapshot,
+} from "firebase/firestore";
 import { db } from "../firebase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Home() {
   const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+
+  // useEffect로 데이터 조회 결과를 변수명 comments에 할당
+  const getComments = async () => {
+    const q = query(collection(db, "comments"), orderBy("date", "desc"), limit(5));
+
+    onSnapshot(q, querySnapshot => {
+      // const querySnapshot = await getDocs(q);
+      // console.log(querySnapshot);
+      console.log(querySnapshot.docs);
+      const comments = [];
+      const commentsArray = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+      setComments(commentsArray);
+    });
+  };
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  console.log(comments);
 
   const handleChange = e => {
     setComment(e.target.value);
@@ -21,6 +62,7 @@ function Home() {
 
       console.log("다음 글이 추가되었습니다 : ", docRef.id);
       setComment("");
+      // getComments();
     } catch (e) {
       console.error("글 추가 시 에러가 발생했습니다.", e);
     }
@@ -29,9 +71,8 @@ function Home() {
   return (
     <>
       <Typography variant="h2" component="h2">
-        Home{" "}
+        Home
       </Typography>
-
       <Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
         <TextField
           fullWidth
@@ -50,8 +91,21 @@ function Home() {
           글쓰기
         </Button>
       </Box>
-
       <Divider sx={{ my: 3 }} />
+      <Typography variant="h4" component="h3">
+        Comments
+      </Typography>
+      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+        {/* commentsArray의 값을 ListItem으로 출력 */}
+        {comments.map(item => (
+          <ListItem key={item.id} alignItems="flex-start" divider>
+            <ListItemText
+              primary={item.comment}
+              secondary={item.date?.toDate ? item.date.toDate().toLocaleString() : "작성시간 없음"}
+            />
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 }
